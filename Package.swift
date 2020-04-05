1a03f9c1881cf8415939c8842449b23bb9101be1
Mathias Quintero <me@quintero.io>
2020-04-05 11:20:28 +0200

    updating graphql

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
                  targets: ["GraphZahl"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/nerdsupremacist/GraphQL.git", from: "0.12.1-beta.3"),
+        .package(url: "https://github.com/nerdsupremacist/GraphQL.git", from: "0.12.1-beta.4"),
         .package(url: "https://github.com/nerdsupremacist/Runtime.git", from: "2.1.2-beta.6"),
         .package(url: "https://github.com/nerdsupremacist/ContextKit.git", from: "0.2.1"),
     ],
