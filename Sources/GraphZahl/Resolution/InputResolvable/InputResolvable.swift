0a9bc92a5c14178953ffd7f9523cdd927dd30e3a
Mathias Quintero <me@quintero.io>
2020-04-14 10:20:46 +0200

    Replacing init with static create function in inputs to make subclassing work

--- Sources/GraphZahl/Resolution/InputResolvable/InputResolvable.swift
+++ Sources/GraphZahl/Resolution/InputResolvable/InputResolvable.swift
@@ -5,5 +5,5 @@ import GraphQL
 public protocol InputResolvable: ValueResolvable {
     static func resolve(using context: inout Resolution.Context) throws -> GraphQLInputType
 
-    init(map: Map) throws
+    static func create(from map: Map) throws -> Self
 }
