c44fa667408903abe10039c12d015ea520d0cbc8
Mathias Quintero <me@quintero.io>
2020-04-14 10:21:33 +0200

    Adding version of get or put with closure instead of autoclosure

--- Sources/GraphZahl/Utils/Dictionary+getOrPut.swift
+++ Sources/GraphZahl/Utils/Dictionary+getOrPut.swift
@@ -4,6 +4,10 @@ import Foundation
 extension Dictionary {
 
     mutating func getOrPut(_ key: Key, default defaultValue: @autoclosure () throws -> Value) rethrows -> Value {
+        return try getOrPut(key) { try defaultValue() }
+    }
+
+    mutating func getOrPut(_ key: Key, default defaultValue: () throws -> Value) rethrows -> Value {
         guard let value = self[key] else {
             let value = try defaultValue()
             self[key] = value
