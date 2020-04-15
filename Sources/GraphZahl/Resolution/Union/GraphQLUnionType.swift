e491540801cb9bfb84e4105e6fb6484f426af283
Mathias Quintero <me@quintero.io>
2020-04-15 09:46:52 +0200

    Fixing issues when with other union type cases

--- Sources/GraphZahl/Resolution/Union/GraphQLUnionType.swift
+++ Sources/GraphZahl/Resolution/Union/GraphQLUnionType.swift
@@ -47,15 +47,36 @@ extension GraphQLUnion {
         guard let cases = caseTypes[unsafeBitCast(Self.self, to: Int.self)] else {
             throw GraphQLUnionError.resolveOnObjectCalledBeforeRegisteringType(type: Self.self)
         }
+
         let bits = Int(ceil(log2(Double(cases.count))))
         let caseIndex = withUnsafeBytes(of: self) { Int($0.last!.mostSignificant(bits)) }
-        let object = withUnsafeBytes(of: self) { $0.baseAddress!.unsafeLoad(as: cases[caseIndex]) as! OutputResolvable }
+        let object = withUnsafeBytes(of: self) { bytes -> OutputResolvable in
+            let mutable = bytes.mutableCopy()
+            mutable.storeBytes(of: bytes.last!.zeroMostSignificant(bits), toByteOffset: bytes.count - 1, as: UInt8.self)
+            return UnsafeRawPointer(mutable.baseAddress!).unsafeLoad(as: cases[caseIndex]) as! OutputResolvable
+        }
+
         return try object.resolve(source: source, arguments: arguments, context: context, eventLoop: eventLoop)
     }
 
 }
 
+extension UnsafeRawBufferPointer {
+
+    fileprivate func mutableCopy() -> UnsafeMutableRawBufferPointer {
+        let pointer = UnsafeMutableRawBufferPointer.allocate(byteCount: count, alignment: MemoryLayout<UInt8>.alignment)
+        pointer.copyMemory(from: self)
+        return pointer
+    }
+
+}
+
 extension UInt8 {
+
+    fileprivate func zeroMostSignificant(_ bits: Int) -> UInt8 {
+        let shape = UInt8.max >> bits
+        return self & shape
+    }
 
     fileprivate func mostSignificant(_ bits: Int) -> UInt8 {
         let shift = Self(MemoryLayout<UInt8>.size * 8 - bits)
